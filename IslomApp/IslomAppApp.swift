3f764dea2225d58d5a899c2d835cf25c8ec82a2e
asadbek824 <yoldoshev5007@icloud.com>
2024-08-01 00:57:30 +0500

    Initial Commit

---
+++ IslomApp/IslomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IslomAppApp.swift
+//  IslomApp
+//
+//  Created by Asadbek Yoldoshev on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct IslomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
